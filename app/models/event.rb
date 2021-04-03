94ac39fd7c2d5b2c308f7768a556f0d7bd5c006b
davidauza.engineer <davidauza01@gmail.com>
2021-04-03 11:12:40 -0500

    Create event model

---
+++ app/models/event.rb
@@ -0,0 +1,13 @@
+class Event < ApplicationRecord
+  validates_presence_of :title, :start_datetime, :location
+  validates :title, length: { minimum: 3 }
+  validate :start_datetime_cannot_be_in_the_past
+
+  private
+
+  def start_datetime_cannot_be_in_the_past
+    if start_datetime.present? && start_datetime < Time.current
+      errors.add(:start_datetime, "can't be in the past")
+    end
+  end
+end
