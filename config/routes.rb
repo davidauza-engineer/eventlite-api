0c16044b64a9b7854d4f6e837d523e8866a1e410
davidauza.engineer <davidauza01@gmail.com>
2021-04-03 11:24:57 -0500

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'events#index'
+  root 'api/v1/events#index'
 
-  resources :events
+  namespace :api do
+    namespace :v1 do
+      resources :events
+    end
+  end
 end
